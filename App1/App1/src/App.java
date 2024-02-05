79135045c294c29243b6ec302da8a20df9f68576
Ainur Elisyah <ainurelisyahmb@gmail.com>
2024-02-05 15:09:49 +0800

    First commit

---
+++ App1/App1/src/App.java
@@ -0,0 +1,28 @@
+public class App {
+    public static void main (String[] args) {
+        System.out.println("Hello, World." + args[0] + "" + args[1]);
+        second();
+        SecondClass secondClass = new SecondClass();
+        secondClass.second();
+        if(args[1].equals("Mar")){
+            SecondClass.third();
+        }
+        
+    }
+
+    public static void second(){
+        System.out.println("second");
+    }
+}
+
+class SecondClass {
+
+    void second(){
+        System.out.println("second in a class");
+    }
+
+    static void third(){
+        System.out.println("third in a class");
+    }
+}
+
